8d7418ca14098fa993b57f0fd5f351180df302ce
Anh Nguyen <vietanh.nguyen@maritimesoftware.de>
2026-08-25 09:03:19 +0700

    Submit Solution

--- DailySolution/2026/08/25/Solution.cs
+++ DailySolution/2026/08/25/Solution.cs
@@ -1,5 +1,3 @@
-namespace LeetCodeDaily20260825;
-
 public class Solution {
     public int MissingMultiple(int[] nums, int k) {
         bool[] dict = new bool[101];
@@ -8,8 +6,9 @@ public class Solution {
             dict[nums[i]] = true;
         }
 
-        for (int i = 0; i < 200/k; i++)
+        for (int i = 1; i < 1000/k; i++)
         {
+            if (i * k >= 101) return i * k;
             if (!dict[i * k])
             {
                 return i * k;
